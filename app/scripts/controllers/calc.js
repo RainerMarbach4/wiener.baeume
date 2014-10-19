ccc8ead8e014539e6f0dde72d43cbb031b1248fc
redt6a <rainer@langheiter.com>
2014-10-19 16:43:36 +0200

    map: redt6a

--- app/scripts/controllers/calc.js
+++ app/scripts/controllers/calc.js
@@ -12,14 +12,12 @@ angular.module('wienerbaeumeApp')
         $scope.art = '+';
         $scope.zahl1 = 1;
         $scope.zahl2 = 1;
-        $scope.ergebnis = $scope.zahl1 * $scope.zahl2;
-
 
         $scope.operators = ['+', '-', '*', '/'];
         $scope.selectedOperator = $scope.operators[0];
 
         $scope.calculate = function() {
-            var A = parseInt($scope.zahl1);
+            var A = $scope.zahl1;
             var B = parseInt($scope.zahl2);
             var C = 0;
 
